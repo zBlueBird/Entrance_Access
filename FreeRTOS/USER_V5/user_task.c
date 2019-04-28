a1e5da269c49ff5ea96b6fb7ee5347129ae9675c
yuyin <32382439+ZhangYuyinGItHub@users.noreply.github.com>
2019-04-28 22:08:22 +0800

    [Entrance] add oled module for entrance access

--- FreeRTOS/USER_V5/user_task.c
+++ FreeRTOS/USER_V5/user_task.c
@@ -2,6 +2,7 @@
 #include "led.h"
 #include "usart1.h"
 #include "keyscan.h"
+#include "oled.h"
 
 #include  "FreeRTOS.h"
 #include "task.h"
@@ -63,9 +64,24 @@ void app_send_msg(APP_MsgType type, uint8_t len, void *p_msg_value)
 
 void user_task2(void *pvParamters)
 {
+	  uint8_t t=0;
+	  OLED_Init();			//初始化OLED  
+		OLED_Clear()  	; 
     while (1)
     {
-        LED1(ON);
+        OLED_ShowCHinese(30,0,0);//元
+		OLED_ShowCHinese(48,0,1);//器
+		OLED_ShowCHinese(66,0,2);//科
+		OLED_ShowCHinese(84,0,3);//技
+		OLED_ShowString(0,3,"1.3' OLED TEST");
+		//OLED_ShowString(8,2,"ZHONGJINGYUAN");  
+	 //	OLED_ShowString(20,4,"2014/05/01");  
+		OLED_ShowString(0,6,"ASCII:");  
+		OLED_ShowString(63,6,"CODE:");  
+		OLED_ShowChar(48,6,t);//显示ASCII字符	   
+		t++;
+		if(t>'~')t=' ';
+		OLED_ShowNum(103,6,t,3,16);//显示ASCII字符的码值 	
         Delay(0x6FFF0);
         LED1(OFF);
         Delay(0x6FFF0);
